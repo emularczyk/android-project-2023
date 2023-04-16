fe16c8053f55e9b6b9ab62e09b9d9bdf67ca09d5
Mateusz Wołek <sekwoja5@gmail.com>
2023-04-16 22:45:45 +0200

    Mess.

--- app/src/main/java/com/calendar/MainActivity.java
+++ app/src/main/java/com/calendar/MainActivity.java
@@ -3,7 +3,6 @@ package com.calendar;
 import static android.content.ContentValues.TAG;
 import static com.calendar.CalendarUtils.convertDateStringToRegardlessOfTheYear;
 import static com.calendar.CalendarUtils.getCurrentDateString;
-import static com.calendar.CalendarUtils.getMiliseconds;
 import static com.calendar.CalendarUtils.selectedDate;
 
 import android.app.DatePickerDialog;
@@ -16,12 +15,11 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.CalendarView;
-import android.widget.DatePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -42,30 +40,57 @@ public class MainActivity extends AppCompatActivity {
     private final ArrayList<Event> eventList = new ArrayList<>();
     private Adapter adapter;
     private RecyclerView recyclerView;
-    private CalendarView calendarView;
-    private DatePickerDialog.OnDateSetListener dateSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        calendarView = findViewById(R.id.calendarView);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.calendarFragmentContainer, MonthlyViewFragment.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("name") // name can be null
+                .commit();
+
         databaseReference = FirebaseDatabase.getInstance().getReference("Calendar");
         setEventRecyclerView();
-        notifyChange();
-
-        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
-            CalendarUtils.selectedDate = LocalDate.of(year, month + 1, dayOfMonth);
-            notifyChange();
-        });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        calendarView.setDate(getMiliseconds(selectedDate));
-        notifyChange();
+    public void weeklyAction(View view) {
+        startActivity(new Intent(this, WeekViewActivity.class));
+    }
+
+    public void createEvent(View view) {
+        startActivity(new Intent(this, CreateEventActivity.class));
+    }
+
+    public void setEventRecyclerView() {
+        Log.i("Events", String.valueOf(eventList.size()));
+        adapter = new Adapter(eventList, getCurrentDateString());
+        recyclerView = findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this, DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(adapter);
+    }
+
+    public void notifyChange() {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                eventList.clear();
+                getEventsFromSnapshot(dataSnapshot, getCurrentDateString());
+                getEventsFromSnapshot(dataSnapshot, convertDateStringToRegardlessOfTheYear(getCurrentDateString()));
+                adapter = new Adapter(eventList, getCurrentDateString());
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "calendarClicked - onCancelled", error.toException());
+            }
+        });
     }
 
     @Override
@@ -82,12 +107,9 @@ public class MainActivity extends AppCompatActivity {
                 DatePickerDialog datePicker = new DatePickerDialog(
                         this,
                         android.R.style.Theme_Light_Panel,
-                        new DatePickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                                CalendarUtils.selectedDate = LocalDate.of(year, month + 1, day);
-                                onResume();
-                            }
+                        (datePicker1, year, month, day) -> {
+                            CalendarUtils.selectedDate = LocalDate.of(year, month + 1, day);
+                            onResume();
                         },
                         selectedDate.getYear(),
                         selectedDate.getMonth().getValue() - 1,
@@ -101,41 +123,6 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    public void weeklyAction(View view) {
-        startActivity(new Intent(this, WeekViewActivity.class));
-    }
-
-    public void createEvent(View view) {
-        startActivity(new Intent(this, CreateEventActivity.class));
-    }
-
-    private void setEventRecyclerView() {
-        adapter = new Adapter(eventList, getCurrentDateString());
-        recyclerView = findViewById(R.id.recyclerView);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
-        recyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this, DividerItemDecoration.VERTICAL));
-    }
-
-    private void notifyChange() {
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                eventList.clear();
-                getEventsFromSnapshot(dataSnapshot, getCurrentDateString());
-                getEventsFromSnapshot(dataSnapshot, convertDateStringToRegardlessOfTheYear(getCurrentDateString()));
-                adapter = new Adapter(eventList, getCurrentDateString());
-                recyclerView.setAdapter(adapter);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.e(TAG, "calendarClicked - onCancelled", error.toException());
-            }
-        });
     }
 
     private void getEventsFromSnapshot(DataSnapshot dataSnapshot, String fromDate) {
---
+++ app/src/main/java/com/calendar/MonthlyViewFragment.java
@@ -0,0 +1,42 @@
+package com.calendar;
+
+import static com.calendar.CalendarUtils.getMilliseconds;
+import static com.calendar.CalendarUtils.selectedDate;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+import java.time.LocalDate;
+
+public class MonthlyViewFragment extends Fragment {
+
+    private CalendarView calendarView;
+
+    @Override
+    public void onResume() {
+        calendarView.setDate(getMilliseconds(selectedDate));
+        ((MainActivity)getActivity()).notifyChange();
+        super.onResume();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_monthly_view, container, false);
+        calendarView = view.findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener((dateView, year, month, dayOfMonth) -> {
+            CalendarUtils.selectedDate = LocalDate.of(year, month + 1, dayOfMonth);
+            ((MainActivity) getActivity()).notifyChange();
+        });
+
+        return view;
+    }
+}
